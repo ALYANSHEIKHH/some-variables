3a861df639ccba37ba177bf2c6cf7dcfc4c8350b
ALYANSHEIKHH <alyansheikhh123@gmail.com>
2024-03-22 10:43:35 +0000

    using some variable

---
+++ mainnn.js
@@ -0,0 +1,4 @@
+var userName = "AlyanSheikhh"; //string
+var age = 18; // Number
+var issStudent = false; // boolean
+var radomValue = "https://www.linked.com/in/alyansheikhh"; // any
---
+++ mainnn.ts
@@ -0,0 +1,4 @@
+let userName: string = "AlyanSheikhh"; //string
+let age: number = 18; // Number
+let issStudent: boolean = false; // boolean
+let radomValue: any = "https://www.linked.com/in/alyansheikhh"; // any
